1340cc4b46867e7bcd8fa40155e380d745c4b536
Jacob Tran <jacobtran@gmail.com>
2018-01-10 20:13:20 -0800

    add day 2 files

---
+++ day_2/review.js
@@ -0,0 +1,15 @@
+let counter = 0;
+
+while (counter <= 10) {
+  console.log(`${counter}: Hello`);
+  counter++;
+}
+
+
+// for loop
+// for (init; condition; iterator)
+for (let counter = 0; counter <= 10; counter++) {
+  if (counter != 4) {
+    console.log(`${counter}: Hello`);
+  }
+}
